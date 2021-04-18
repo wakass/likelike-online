bbe1c31065cdfe7cead289fa62fa34bdb13bd974
waka <p.c.spruijtenburg@utwente.nl>
2021-04-18 17:14:06 +0200

    Added music for the majority of rooms

--- data.js
+++ data.js
@@ -49,6 +49,7 @@ module.exports.ROOMS = {
         avatarScale: 2,
         pageBg: "#ab5236",
         area: "lobbyArea.png",
+        music: "lobby.ogg",
         tint: "#fdeac8",
         bubblesY: 44,
         spawn: [38, 83, 100, 100],
@@ -74,7 +75,7 @@ module.exports.ROOMS = {
             hf800ff: { cmd: "enter", room: "garden", label: "Garden", point: [46, 84], enterPoint: [104, 98], obstacle: false },
             h79077c: { cmd: "enter", room: "barbarella", label: "Barbarella", point: [74, 84], enterPoint: [8, 21], obstacle: false },
             ha924ad: { cmd: "enter", room: "neon", label: "Neon", point: [96, 84], enterPoint: [104, 98], obstacle: false },
-            h8b348d: { cmd: "enter", room: "makeUpRoom", label: "Make up room", point: [111, 84], enterPoint: [104, 98], obstacle: false },
+            h8b348d: { cmd: "enter", room: "makeupRoom", label: "Make up room", point: [111, 84], enterPoint: [104, 98], obstacle: false },
             h009688: { cmd: "enter", room: "lunarStage", label: "", point: [22, 75], enterPoint: [104, 98], obstacle: false },
         }
     },
@@ -111,7 +112,50 @@ module.exports.ROOMS = {
         spawn: [38, 80, 100, 100],
         areaColors: {
             //h will be replaced by #
-            hff008b: { cmd: "enter", room: "neon", label: "Neon", point: [64, 50], enterPoint: [104, 98], obstacle: false },
+            hff00ff: { cmd: "enter", room: "neon", label: "Neon", point: [64, 50], enterPoint: [104, 98], obstacle: false },
+        }
+    },
+
+    makeupRoom: {
+        bg: "makeuproomBg.png",
+        frames: 2,
+        frameDelay: 30,
+        avatarScale: 1,
+        pageBg: "#ab5236",
+        area: "makeuproomArea.png",
+        tint: "#fdeac8",
+        bubblesY: 44,
+        spawn: [5, 15, 21, 28],
+        areaColors: {
+            //h will be replaced by #
+            hff0000: { cmd: "enter", room: "neon", label: "Neon", point: [121, 77], enterPoint: [104, 98], obstacle: false },
+            h00ff00: { cmd: "enter", room: "theStateOfMaine", label: "State of Plupluplu", point: [85, 35], enterPoint: [104, 98], obstacle: false },
+            h0000ff: { cmd: "enter", room: "barbarella", label: "Barbarella", point: [33, 54], enterPoint: [104, 98], obstacle: false },
+        }
+    },
+
+    neon: {
+        bg: "neonBg.png",
+        frames: 2,
+        frameDelay: 30,
+        avatarScale: 1,
+        pageBg: "#ab5236",
+        area: "neonArea.png",
+        music: "neon.ogg",
+        tint: "#fdeac8",
+        bubblesY: 44,
+        spawn: [15, 88, 37, 99],
+        areaColors: {
+            //h will be replaced by #
+            hff0000: { cmd: "enter", room: "garden", label: "Voluptuous garden", point: [12, 92], enterPoint: [104, 98], obstacle: false },
+            h00ff00: { cmd: "enter", room: "barbarella", label: "Barbarella", point: [77, 90], enterPoint: [62, 17], obstacle: false },
+            h0000ff: { cmd: "enter", room: "makeupRoom", label: "Makeup room", point: [119, 94], enterPoint: [104, 98], obstacle: false },
+        },
+
+        things: {
+            //sprite spreadsheets only 1 row ok?
+            wall1: { file: "neonWall1.png", frames: 1, frameDelay: 1, position: [43, 100], label: "" },
+            wall2: { file: "neonWall2.png", frames: 1, frameDelay: 1, position: [84, 100], label: "" }
         }
     },
 
@@ -122,16 +166,17 @@ module.exports.ROOMS = {
         avatarScale: 1,
         pageBg: "#ab5236",
         area: "barbarellaArea.png",
+        music: "barbarella.ogg",
         tint: "#fdeac8",
         bubblesY: 44,
         spawn: [5, 15, 21, 28],
         areaColors: {
             //h will be replaced by #
-            hff0000: { cmd: "enter", room: "makeupRoom", label: "???", point: [120, 20], enterPoint: [104, 98], obstacle: false },
-            h00ff00: { cmd: "enter", room: "garden", label: "???", point: [118, 88], enterPoint: [104, 98], obstacle: false },
-            h0000ff: { cmd: "enter", room: "theStateOfMaine", label: "???", point: [14, 95], enterPoint: [104, 98], obstacle: false },
-            hff00ff: { cmd: "enter", room: "neon", label: "???", point: [59, 15], enterPoint: [104, 98], obstacle: false },
-            h00ffff: { cmd: "enter", room: "makeupRoom", label: "???", point: [55, 87], enterPoint: [104, 98], obstacle: false },
+            hff0000: { cmd: "enter", room: "makeupRoom", label: "???", point: [113, 19], enterPoint: [104, 98], obstacle: false },
+            h00ff00: { cmd: "enter", room: "garden", label: "???", point: [117, 86], enterPoint: [104, 98], obstacle: false },
+            h0000ff: { cmd: "enter", room: "theStateOfMaine", label: "???", point: [16, 95], enterPoint: [104, 98], obstacle: false },
+            hff00ff: { cmd: "enter", room: "neon", label: "???", point: [56, 13], enterPoint: [104, 98], obstacle: false },
+            h00ffff: { cmd: "enter", room: "makeupRoom", label: "???", point: [58, 84], enterPoint: [104, 98], obstacle: false },
         }
     },
 };
